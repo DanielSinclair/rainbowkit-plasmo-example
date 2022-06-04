fde1bf39572fa0a6a8c48ecaab3a2bc36a469c83
Daniel Sinclair <daniel@rainbow.me>
2022-06-03 23:52:14 -0400

    added options entrypoint

---
+++ options/index.tsx
@@ -0,0 +1,14 @@
+
+import RainbowKit from "../components/RainbowKit"
+import Popup from "../components/Popup"
+
+// Options Entrypoint
+const Index = () => {
+  return (
+    <RainbowKit>
+      <Popup/>
+    </RainbowKit>
+  )
+}
+
+export default Index
